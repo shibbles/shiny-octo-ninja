9d11154e233819ad915ed60edfb5b61ecef8271f
shibbles <siobhanw2@hotmail.com>
2014-07-06 22:57:26 +1000

    HTML / CSS updates

---
+++ js/octoScript.js
@@ -0,0 +1,11 @@
+$( document ).ready(function() {
+	/*var options = {
+	  socketRadius: 4, // radius of the circle in which the eye's pupil can move
+	  stickToSocket: false, // constraint the eye to the perimeter of the circle
+	  bindMouseMove: true,
+	  bindTouchMove: false,
+	}
+
+	var leftEye  = new Eye ('leftEyePupil', options);
+	var rightEye  = new Eye ('rightEyePupil', options);*/
+});
