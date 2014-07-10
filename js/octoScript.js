07bf1d743c88ba09c5dd6f5638d9b9d38bb161da
shibbles <siobhanw2@hotmail.com>
2014-07-10 22:13:15 +1000

    Arm movement

--- js/octoScript.js
+++ js/octoScript.js
@@ -1,4 +1,40 @@
 $( document ).ready(function() {
+
+	var movementXLeft;
+	var movementXRight;
+	var movementY;
+
+	var width = $(window).width();
+	var height = $(window).height();
+
+	console.log(width + ", " + height);
+
+	$(document).mousemove(function(event){
+		//console.log(event.pageX + ", " + event.pageY); 
+
+	  movementXLeft = event.pageX / width * 400 - 20;
+	  movementXRight = event.pageX / width * 400 + 20;
+	  movementY = event.pageY / height * 300;
+
+	  if (movementXLeft < 20) {
+	  	movementXLeft = 20;
+	  	movementXRight = 80;
+	  }
+	  if (movementXRight > 380) {
+	  	movementXRight = 380;
+	  	movementXLeft = 340;
+	  }
+	  if (movementY < 20) {
+	  	movementY = 20;
+	  }
+
+
+
+
+	  $(".leftArm").attr("d", "M80,300 Q0,200 " + movementXLeft + "," + movementY);
+	  $(".rightArm").attr("d", "M320,300 Q400,200 " + movementXRight + "," + movementY);
+	 // console.log(movementY);
+	}); 
 	
 	//Turn octoArms svg element into a Snap object
 	var octoArms = Snap("#octoArms");
@@ -11,12 +47,14 @@ $( document ).ready(function() {
 	    fill: "none",
 	    stroke: "#4398CC",
 	    strokeWidth: 20,
+	    class: "leftArm"
 	});
 
 	rightArm.attr({
 	    fill: "none",
 	    stroke: "#4398CC",
 	    strokeWidth: 20,
-	});
+	   	class: "rightArm"
+	}); 
         
 });
